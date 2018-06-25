0fcd0ee1dbcc43e74a65a7c4723209fed2bed4c6
Ryan Lee <gxlhybh@gmail.com>
2018-06-25 15:15:38 +0800

    Fix open RPK bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -22,5 +23,57 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
+*/
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2017,
+    'sourceType': 'module'
+  },
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'no-console': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'indent': [
+      'error',
+      'tab'
+    ],
+    extends: 'standard',
+    globals: {
+      __static: true
+    },
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'double'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
   }
 }
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue";
+import axios from "axios";
 
-import App from './App'
-import router from './router'
-import store from './store'
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
-import 'semantic-ui-offline/semantic.min.css'
+import App from "./App";
+// import router from "./router";
+// import store from "./store";
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+import "semantic-ui-offline/semantic.min.css";
 // use
-Vue.use(mavonEditor)
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
+Vue.use(mavonEditor);
+if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 window.total_vue = new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
-}).$mount('#app')
+	components: {App},
+	// router,
+	// store,
+	template: "<App/>"
+}).$mount("#app");
