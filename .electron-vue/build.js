0f799df8bb64999eb6ed152735cfdc3885ffad61
Ryan Lee <gxlhybh@gmail.com>
2018-07-07 01:18:00 +0800

    Build for arch ia32

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -2,7 +2,7 @@
 
 process.env.NODE_ENV = 'production'
 
-const { say } = require('cfonts')
+const {say} = require('cfonts')
 const chalk = require('chalk')
 const del = require('del')
 const packager = require('electron-packager')
@@ -10,6 +10,7 @@ const webpack = require('webpack')
 const Multispinner = require('multispinner')
 
 const buildConfig = require('./build.config')
+const buildConfigIa32 = require('./build.config.ia32')
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
 const webConfig = require('./webpack.web.config')
@@ -19,9 +20,13 @@ const errorLog = chalk.bgRed.white(' ERROR ') + ' '
 const okayLog = chalk.bgBlue.white(' OKAY ') + ' '
 const isCI = process.env.CI || false
 
-if (process.env.BUILD_TARGET === 'clean') clean()
-else if (process.env.BUILD_TARGET === 'web') web()
-else build()
+if (process.env.BUILD_TARGET === 'clean') {
+  clean()
+} else if (process.env.BUILD_TARGET === 'web') {
+  web()
+} else {
+  build()
+}
 
 function clean () {
   del.sync(['build/*', '!build/icons', '!build/icons/icon.*'])
@@ -73,18 +78,19 @@ function build () {
 function pack (config) {
   return new Promise((resolve, reject) => {
     webpack(config, (err, stats) => {
-      if (err) reject(err.stack || err)
-      else if (stats.hasErrors()) {
+      if (err) {
+        reject(err.stack || err)
+      } else if (stats.hasErrors()) {
         let err = ''
 
         stats.toString({
           chunks: false,
           colors: true
         })
-        .split(/\r?\n/)
-        .forEach(line => {
-          err += `    ${line}\n`
-        })
+          .split(/\r?\n/)
+          .forEach(line => {
+            err += `    ${line}\n`
+          })
 
         reject(err)
       } else {
@@ -98,9 +104,19 @@ function pack (config) {
 }
 
 function bundleApp () {
-  packager(buildConfig, (err, appPaths) => {
+  if (process.env.BUILD_ARCH !== 'ia32') {
+    packager(buildConfig, (err, appPaths) => {
+      if (err) {
+        console.log(`\n${errorLog}${chalk.yellow('`electron-packager`')} says...\n`)
+        console.log(err + '\n')
+      } else {
+        console.log(`\n${doneLog}\n`)
+      }
+    })
+  }
+  packager(buildConfigIa32, (err, appPaths) => {
     if (err) {
-      console.log(`\n${errorLog}${chalk.yellow('`electron-packager`')} says...\n`)
+      console.log(`\n${errorLog}${chalk.yellow('`electron-packager`')} for ia32 says...\n`)
       console.log(err + '\n')
     } else {
       console.log(`\n${doneLog}\n`)
@@ -126,9 +142,13 @@ function greeting () {
   const cols = process.stdout.columns
   let text = ''
 
-  if (cols > 85) text = 'lets-build'
-  else if (cols > 60) text = 'lets-|build'
-  else text = false
+  if (cols > 85) {
+    text = 'lets-build'
+  } else if (cols > 60) {
+    text = 'lets-|build'
+  } else {
+    text = false
+  }
 
   if (text && !isCI) {
     say(text, {
@@ -136,6 +156,8 @@ function greeting () {
       font: 'simple3d',
       space: false
     })
-  } else console.log(chalk.yellow.bold('\n  lets-build'))
+  } else {
+    console.log(chalk.yellow.bold('\n  lets-build'))
+  }
   console.log()
 }
